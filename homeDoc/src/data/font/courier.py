84c961ea3d47a5b84961b8c98e012cb45dfbfbfc
guezdav <guezdav@9d9a54a2-ea46-af66-5edd-d513bd7b780c>
2010-08-17 22:24:42 +0000

    minor change

--- homeDoc/src/data/font/courier.py
+++ homeDoc/src/data/font/courier.py
@@ -5,7 +5,7 @@ from _header import *
 fpdf_charwidths['courier']={}
 
 for i in range(255+1):
-  fpdf_charwidths['courier'][chr(i)]=600;
+    fpdf_charwidths['courier'][chr(i)]=600;
   
 fpdf_charwidths['courierB']=fpdf_charwidths['courier'];
 fpdf_charwidths['courierI']=fpdf_charwidths['courier'];
