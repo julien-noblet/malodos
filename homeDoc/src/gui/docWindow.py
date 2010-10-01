9bf11bcbac54773c1efc3ebee4268c6f5000eb11
guezdav <guezdav@9d9a54a2-ea46-af66-5edd-d513bd7b780c>
2010-10-01 01:03:21 +0000

    start implementing the pan/zoom function in the image viewer

--- homeDoc/src/gui/docWindow.py
+++ homeDoc/src/gui/docWindow.py
@@ -20,6 +20,7 @@ import wx.lib.mixins.rubberband
 class docWindow(wx.Window) :
     center=[0.5,0.5]
     window=[1.0,1.0]
+    dragLastPos = None
     #===========================================================================
     # constructor (gui building)
     #===========================================================================
@@ -48,10 +49,29 @@ class docWindow(wx.Window) :
         self.Bind(wx.EVT_BUTTON, self.actionNextImage, self.btRight)
         #self.Bind(wx.EVT_BUTTON, self.actionGetZoom, self.btZoom)
         self.Bind(wx.EVT_SIZE,self.onResize)
+        self.Bind(wx.EVT_MOUSEWHEEL,self.onMouseWheelEvent)
+        self.Bind(wx.EVT_MOTION,self.onMouseMotion)
+        self.Bind(wx.EVT_LEFT_UP,self.onMouseLeftUp)
         
         self.canvas.SetSize(wx.Size(200,300))
         self.panel.SetSizerAndFit(self.totalWin)
 
+    def onMouseLeftUp(self,event):
+        self.dragLastPos = None
+    def onMouseWheelEvent(self,event):
+        rot = -float(event.GetWheelRotation()) / event.GetWheelDelta() * 0.05
+        self.window[0] += rot;
+        self.window[1] += rot;
+        if self.window[0]<0 : self.window[0]=0;
+        if self.window[1]<0 : self.window[1]=0;
+        if self.window[0]>1 : self.window[0]=1;
+        if self.window[1]>1 : self.window[1]=1;
+        self.showCurrentImage()
+    def onMouseMotion(self,event):
+        #if not event.Dragging() : return
+        if not self.dragLastPos : self.dragLastPos = event.GetPosition();
+        delta  = event.GetPosition() - self.dragLastPos
+        print delta
     def actionGetZoom(self,event):
         pass
         #R = wx.lib.mixins.rubberband.RubberBand(self.canvas)
@@ -70,14 +90,32 @@ class docWindow(wx.Window) :
             pil_image = data.theData.get_image()
             self.img = wx.EmptyImage(pil_image.size[0],pil_image.size[1])
             self.img.SetData(pil_image.convert("RGB").tostring())
-        if not self.img : return    
-        #img = data.theData.get_image().convertToWx()
+        if not self.img : return
+        
+        # build and test the viewing rect
+        viewRect = wx.Rect((self.center[0]-self.window[0]/2)*self.img.GetWidth() \
+                          ,(self.center[1]-self.window[1]/2)*self.img.GetHeight() \
+                          ,self.window[0]*self.img.GetWidth() ,self.window[1]*self.img.GetHeight())
+        if viewRect.x<0 : viewRect.x=0;
+        if viewRect.y<0 : viewRect.y=0;
+        if viewRect.x>= self.img.GetWidth() : viewRect.x = self.img.GetWidth() -1
+        if viewRect.y>= self.img.GetHeight() : viewRect.y = self.img.GetWidth() -1
+        if viewRect.width<1 : viewRect.width = 1 
+        if viewRect.height<1 : viewRect.height = 1
+        if viewRect.width > self.img.GetWidth() : viewRect.width = self.img.GetWidth()
+        if viewRect.height > self.img.GetHeight() : viewRect.height = self.img.GetHeight()
+        if viewRect.x + viewRect.width > self.img.GetWidth() : viewRect.x = self.img.GetWidth() - viewRect.width
+        if viewRect.y + viewRect.height > self.img.GetHeight() : viewRect.y = self.img.GetHeight() - viewRect.height
+        
+        theImage = self.img.ConvertToBitmap();
+        theImage = theImage.GetSubBitmap(viewRect)
+        theImage = theImage.ConvertToImage()
 
         # calculating stretching factors
         size_ini = self.canvas.GetSize()
         disp_ini = self.canvas.GetPosition()
         size = self.canvas.GetSize()
-        origSize = self.img.GetSize()
+        origSize = theImage.GetSize()
         # factor to apply in each direction
         factors = [ float(size[0])/origSize[0] , float(size[1])/origSize[1] ]
         # take only the lowest factor and apply to both x and y direction
@@ -93,7 +131,7 @@ class docWindow(wx.Window) :
             displ = [ disp_ini[0] + (size_ini[0] - size[0])/2 , disp_ini[1] + (size_ini[1] - size[1])/2 ]
             self.canvas.SetPosition(displ)
             self.canvas.SetSize( [size[0],size[1] ])
-            self.canvas.SetBitmap(self.img.Scale(size[0],size[1]).ConvertToBitmap())
+            self.canvas.SetBitmap(theImage.Scale(size[0],size[1]).ConvertToBitmap())
     #===========================================================================
     # on prev button click
     #===========================================================================
