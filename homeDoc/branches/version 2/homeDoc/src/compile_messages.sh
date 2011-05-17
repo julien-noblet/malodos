e55b05171be13d7142f8a7be0a257f85c1ffe111
guezdav@gmail.com <guezdav@gmail.com@9d9a54a2-ea46-af66-5edd-d513bd7b780c>
2011-05-17 22:13:34 +0000

    removing branch2

--- homeDoc/branches/version 2/homeDoc/src/compile_messages.sh
+++
@@ -1,1 +0,0 @@
-msgfmt -o locale/fr_FR/LC_MESSAGES/malodos.mo locale/fr_FR/LC_MESSAGES/malodos.po
--- homeDoc/branches/version 2/homeDoc/src/make_po.sh
+++
@@ -1,1 +0,0 @@
-xgettext $(find . -name '*.py' -and -not -name TextCtrlAutoComplete.py -and -not -path '*font*' -and -not -name '*fpdf*') -j -o locale/fr_FR/LC_MESSAGES/malodos.po
