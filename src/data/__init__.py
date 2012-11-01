589035015719d5c4b524147daf9f83fa7e63af6a
guezdav <guezdav@gmail.com>
2012-11-01 22:10:49 +0000

    start implementation of encrypted file feature

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -9,9 +9,30 @@ attached to this project (LICENSE.txt file)
 '''
 import imageData
 import gui.utilities
+import Crypto.Hash.MD5 as md5
+
 theData = imageData.imageData()
 currentPassword=''
 def get_current_password(msg=_('No password defined, please give one')):
-    if currentPassword=='':
-        currentPassword = gui.utilities.ask_password(msg, "", "")
+    global currentPassword
+    cont = len(currentPassword)<10
+    while cont:
+        currentPassword = gui.utilities.ask_password(msg)
+        if len(currentPassword)==0:
+            cont=False
+        elif len(currentPassword)<4:
+            gui.utilities.show_message(_('The password length must be at least 4  char long (at least 8 is recommended). Please gives another one..'))
+        else:
+            M =md5.new()
+            M.update(currentPassword)
+            S1 = M.digest()
+            x=''
+            for i in range(len(currentPassword)-1,-1,-1) : x = x+currentPassword[i]
+            M.update(x)
+            S2 = M.digest()
+            currentPassword = S1+S2
+            cont=False
     return currentPassword
+def clear_current_password():
+    global currentPassword
+    currentPassword=''
