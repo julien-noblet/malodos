63342fdc84b0c2273f9a16df01b5bfc9945ebdaa
guezdav <guezdav@gmail.com>
2012-11-15 14:39:04 +0000

    start implementation of encrypted file/database feature

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -15,30 +15,38 @@ from database import theConfig
 
 theData = imageData.imageData()
 currentPassword=''
-def get_current_password(msg=_('Please gives your password for data encryption/decryption.'),forceAsk=False,checkOld=True):
-    global currentPassword
-    cont = forceAsk or len(currentPassword)<10
-    salt = theConfig.get_param('encryption','salt','')
-    hashed = theConfig.set_param('encryption','hash','')
+def get_password(msg=_('Please gives your password for data encryption/decryption.'),checker=lambda pss:False):
+    cont = True
     while cont:
-        currentPassword = gui.utilities.ask_password(msg)
-        if len(currentPassword)==0:
+        password = gui.utilities.ask_password(msg)
+        if len(password)==0:
             cont=False
-        elif len(currentPassword)<4:
+        elif len(password)<4:
             gui.utilities.show_message(_('The password length must be at least 4  char long (at least 8 is recommended). Please gives another one..'))
         else:
             M =md5.new()
-            M.update(currentPassword)
+            M.update(password)
             S1 = M.digest()
             x=''
-            for i in range(len(currentPassword)-1,-1,-1) : x = x+currentPassword[i]
+            for i in range(len(password)-1,-1,-1) : x = x+password[i]
             M.update(x)
             S2 = M.digest()
-            currentPassword = S1+S2
-            if checkOld and (salt!='' and hashed!='' and bcrypt.hashpw(currentPassword, salt) != hashed):
-                cont = gui.utilities.ask(_('The password does not corresponds to the registered one, do you want to give it back ?..'))
-            else:
-                cont=False
+            password = S1+S2
+            cont=checker(password)       
+    return password
+    
+def get_current_password(msg=_('Please gives your password for data encryption/decryption.'),forceAsk=False,checkOld=True):
+    global currentPassword
+    if not forceAsk and len(currentPassword)>=10 : return currentPassword
+    salt = theConfig.get_param('encryption','salt','')
+    hashed = theConfig.get_param('encryption','hash','')
+    def checker(pss):
+        if checkOld and (salt!='' and hashed!='' and bcrypt.hashpw(pss, salt) != hashed):
+            cont = gui.utilities.ask(_('The password does not corresponds to the registered one, do you want to give it back ?..'))
+        else:
+            cont=False
+        return cont
+    currentPassword = get_password(msg, checker)
     if currentPassword != '':
         salt = bcrypt.gensalt()
         hashed = bcrypt.hashpw(currentPassword, salt)
